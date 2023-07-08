cb16efc4b029c0d054322bbcb7cd76025bc1b5e1
Master Doom <manaskgp2593@kgpian.iitkgp.ac.in>
2023-07-08 09:03:14 +0530

    vercel deployment

--- DjangoDeployment/DjangoDeployment/settings.py
+++ DjangoDeployment/DjangoDeployment/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
